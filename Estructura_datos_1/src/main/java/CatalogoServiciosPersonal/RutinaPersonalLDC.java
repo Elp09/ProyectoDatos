f0e8ee95dbb717213ed51365fcbdf98de567b5c7
elp <leblicqe@gmail.com>
2024-07-29 17:28:41 -0600

    verificacion_agregar_personal_vacia

--- Estructura_datos_1/src/main/java/CatalogoServiciosPersonal/RutinaPersonalLDC.java
+++ Estructura_datos_1/src/main/java/CatalogoServiciosPersonal/RutinaPersonalLDC.java
@@ -1,6 +1,9 @@
 package CatalogoServiciosPersonal;
 
+import javax.swing.JOptionPane;
+
 public class RutinaPersonalLDC {
+
     LDC lista_doble_personal;
 
     public RutinaPersonalLDC() {
@@ -10,19 +13,29 @@ public class RutinaPersonalLDC {
     public RutinaPersonalLDC(LDC lista_doble_personal) {
         this.lista_doble_personal = lista_doble_personal;
     }
-    
-    public NodoLDC crear__nodo_persona(String cedula, String nombre, String primerApellido, String segundoApellido, String direccion, String telefono){
+
+    public NodoLDC crear__nodo_persona(String cedula, String nombre, String primerApellido, String segundoApellido, String direccion, String telefono) {
         Persona persona = new Persona(cedula, nombre, primerApellido, segundoApellido, direccion, telefono);
         return new NodoLDC(persona);
     }
-    
-    public void agregar_personal(String cedula, String nombre, String primerApellido, String segundoApellido, String direccion, String telefono){
-        NodoLDC nuevo_nodo = crear__nodo_persona(cedula, nombre, primerApellido, segundoApellido, direccion, telefono);
-        lista_doble_personal.agregar(nuevo_nodo, null);
+
+    public void agregar_personal(String cedula, String nombre, String primerApellido, String segundoApellido, String direccion, String telefono) {
+        if (usuario_existe(cedula)) {
+            NodoLDC nuevo_nodo = crear__nodo_persona(cedula, nombre, primerApellido, segundoApellido, direccion, telefono);
+            lista_doble_personal.agregar(nuevo_nodo, null);
+            JOptionPane.showMessageDialog(null, "La persona fue registrada con exito");
+        } else {
+            JOptionPane.showMessageDialog(null, "Error, la persona ya esta registrada");
+        }
     }
-    
-    public void mostrar_personal(){
+
+    public void mostrar_personal() {
         lista_doble_personal.mostrar();
     }
-    
+
+    //aqui va la verificacion en base a la cedula de la persona
+    public boolean usuario_existe(String cedula) {
+        return true;
+    }
+
 }
